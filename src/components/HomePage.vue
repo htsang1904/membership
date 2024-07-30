d0d98c00388376aec676fb35f383f346be9ce455
Huỳnh Thanh Sang <111369238+htsang1904@users.noreply.github.com>
2024-07-31 02:18:13 +0700

    update

---
+++ src/components/HomePage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home-page">
+    <div class="header">
+      <div class="member-info"></div>
+      <div class="member-card"></div>
+    </div>
+    <div class="body"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String
+  }
+}
+</script>
+<style lang="scss" scoped>
+.home-page {
+  max-width: 600px;
+  width: 100%;
+  height: 100vh;
+  background-color:#121a48;
+  .header {
+    width: 100%;
+    height: 30%;
+    padding: 14px;
+    box-sizing: border-box;
+    display: flex;
+    .member-info {
+      flex: 3;
+      background-color: aliceblue;
+    }
+    .member-card {
+      flex: 2;
+      background-color: beige;
+    }
+  }
+}
+</style>
