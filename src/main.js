574195267047bea010de839160bd4ecd5ccfa7e1
Huỳnh Thanh Sang <111369238+htsang1904@users.noreply.github.com>
2024-12-24 09:43:42 +0700

    update

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import 'element-ui/lib/theme-chalk/index.css';
 import Flicking from "@egjs/vue-flicking";
 import "@egjs/vue-flicking/dist/flicking.css";
 import "@egjs/flicking-plugins/dist/flicking-plugins.css";
-
+import * as VueGoogleMaps from "vue2-google-maps"
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 Vue.use(Flicking);
